45794f33feca23795108e4446310ba437f2adcc1
Evin O'Shea <eco2000@wildcats.unh.edu>
2018-10-09 18:20:01 -0400

    updated text on pages, change scripts directory to s/ instead of scripts/

--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -33,10 +33,31 @@ class Home extends Component {
 					<br/>
 					<br/>
 					&nbsp;&nbsp;This website is the way that I feel most comfortable publicizing myself. As I can demonstrate experience with it.
-		  		<br/>
+		  		Thank you for spending the time to see who I am!
 					<br/>
 					<br/>
-		  		&nbsp;&nbsp;Thank you for spending the time to see who I am!
+					&nbsp;&nbsp;I am Evin O'Shea. I studied physics as an undergraduate because I love
+					to learn and I wanted to learn how the world works. But, during my time as
+					an undergrad I didn't just learn about physics, I also learned a lot about software development.
+					I worked on the data processing for NASA's Parker Solar Probe-IS☉IS
+					where I learned the ropes of software development from python to git to SQL and even
+					logging and documentation.
+					<br/>
+					<br/>
+					&nbsp;&nbsp;As I finished my schooling, I decided I wanted to be a web developer
+					because I wanted to have a say in building the future of the internet.
+					Though I had worked in software development, I had not used we developement
+					frameworks. I had been writting the back-end scripts. Now, I am working
+					on projects to show my progress and my ability to learn. This website
+					was built with React, and though it is simple, learning a little can go a long
+					way.
+					<br/>
+					<br/>
+					&nbsp;&nbsp;I have been studying various emerging web development technologies, including
+					Ruby on Rails, Docker, Golang, etc. I have learned a lot and I still have
+					more to learn, but I am always ready and excited to learn anything!
+					<br/>
+					<br/>
 				</p>
 			</section>
 		);
@@ -55,12 +76,19 @@ class About extends Component {
 					way to track my work and progress.
 				</p>
 				<p>
-				  &nbsp;&nbsp;Check out the Projects tab for my upcoming and in progress projects.
+				  &nbsp;&nbsp;Check out the Projects tab for my upcoming and in-progress projects.
 					Also look at my &nbsp;
 					<a href="https://github.com/evinosheaforward">
 						github page
 					</a>
 					&nbsp; to look through my projects!
+				</p>
+				<p>
+					&nbsp;&nbsp;This website is hosted by Heroku through their container registry.
+					The project was created in React because it seemed like the best tool
+					for a simple, static website. Though React is great, I have also been
+					learning about Ruby on Rails and D3.js. Check out my Productivity app
+					to see what I have been doing with those tools!
 				</p>
 			</section>
 		);
@@ -76,7 +104,7 @@ class Experience extends Component {
 				</h2>
 				<p>
 				  &nbsp;&nbsp;I have been trained in physics, mathematics, and computer science. I have learned to
-				  make models. I use linear algebra and other schemes to make models. These
+				  make models. I use linear algebra and other mathematical tools to formulate models and to decompose systems. These
 				  models can be used to predict an element of the system, they can be used to
 				  describe and visualize the system, and they can be combined with other models
 				  to create application interfaces.
@@ -85,21 +113,21 @@ class Experience extends Component {
 				  &nbsp;&nbsp;My experience for the last 3 years has been at the data processing center
 				  (Science Operations Center) for &nbsp;
 					<a href="http://parkersolarprobe.jhuapl.edu/News-Center/Show-Article.php?articleID=86">
-						NASA's Parker Solar Probe-ISOIS
+						NASA's Parker Solar Probe-IS☉IS
 					</a>.
 				  &nbsp;We use a postgres database and sqlalchemy to store our telemetry data. We
 				  have asynchronous daemons running that regularly run new data through our processing pipeline.
 				</p>
 				<p>
-				  &nbsp;&nbsp;Our data is inserted to our database regularly using a sqlalchemy wrapper that
-				  I have work on. The daemon will look at a 'todo' file
-				  and will regularly insert all new data and then create all 'raw' data files
-				  that there are new data for. These files are then handed off to the next system.
+				  &nbsp;&nbsp;Our data is inserted into our database regularly using a sqlalchemy wrapper that
+				  I worked on. The daemon looks for new data and inserts new files as they are synced to our server.
+				  Then, the daemon will create all 'raw' data files
+				  that there are new data for. These files are then handed off to an ETL framework.
 				</p>
 				<p>
-				  &nbsp;&nbsp;We use an ETL framework which is a file manager and ETL processer. Our files have
+				  &nbsp;&nbsp;We use an ETL framework which is a file manager and ETL processor. Our files have
 					specifications that fit to NASA standards and we use these specs todo
-					identify the filetype, save the file and then perform all possible
+					identify the filetype, save the file, and then perform all possible
 					ETL's for that file. The system is configurable so that any script can be used for file
 					identification and any scripts can be used for the ETL. The ETL framework
 					was developed at Los Alamos National Lab and is an ongoing internal development.
